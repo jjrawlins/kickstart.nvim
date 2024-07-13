2a29ddcaa6cca46eb3225e0199861c29d609018a
Jayson Rawlins <JaysonJ.Rawlins@gmail.com>
2024-07-12 20:40:14 -0400

    Getting closer to what I like

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+-- disable netrw at the very start of your init.lua
+--
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- optionally enable 24-bit colour
+vim.opt.termguicolors = true
--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -142,7 +142,7 @@ return {
   -- would overwrite `ensure_installed` with the new value.
   -- If you'd rather extend the default config, use the code below instead:
   {
-    "nvim-treesitter/nvim-treesitter",
+    "nvi-treesitter/nvim-treesitter",
     opts = function(_, opts)
       -- add tsx and treesitter
       vim.list_extend(opts.ensure_installed, {
