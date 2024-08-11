8b474952bb5ecacdb20c3526a91f8cfc46945f6c
Jayson Rawlins <JaysonJ.Rawlins@gmail.com>
2024-08-11 10:28:37 -0400

    Adding a cdk workflows projen typescript

--- lua/config/snippets.lua
+++ lua/config/snippets.lua
@@ -12,6 +12,15 @@ local date = function()
   return { os.date("%Y-%m-%d") }
 end
 
+-- Function to convert multiline string to array
+local function multiline_to_array(str)
+  local lines = {}
+  for line in str:gmatch("([^\n]*)\n?") do
+    table.insert(lines, line)
+  end
+  return lines
+end
+
 local get_filename = function()
   local bufname = vim.fn.expand("%:t:r")
   if bufname == "" then
@@ -334,6 +343,286 @@ ls.add_snippets(nil, {
       insert(4),
       text({ "]", "comments: true", "---", "" }),
       insert(0),
+    }),
+    -- New multiline string snippet
+    snip({
+      trig = "cdk-workflows-typescript",
+      namr = "CDK Workflows TypeScript",
+      dscr = "CDK Workflows TypeScript",
+    }, {
+      text(multiline_to_array([==[
+import { execSync } from 'child_process';
+import { AwsCdkTypeScriptApp } from 'projen/lib/awscdk';
+import { GithubWorkflow } from 'projen/lib/github';
+import { JobPermission, JobStep } from 'projen/lib/github/workflows-model';
+
+const githubActionsAwsCredentialsVersion = 'v4';
+const githubActionsCheckoutVersion = 'v4';
+const githubActionsTokenVersion = 'v2';
+const githubActionsSetupNodeVersion = 'v4';
+const nodeVersion = '20.x';
+const githubActionsScriptVersion = 'v6';
+
+export const detectStacks = (): string[] => {
+  try {
+    const cdkListOutput = execSync('yarn cdk list', { encoding: 'utf-8' });
+    const lines = cdkListOutput.trim().split('\n');
+
+    // Filter out non-stack lines
+    const stacks = lines.filter(line =>
+      !line.startsWith('yarn run') &&
+      !line.startsWith('$') &&
+      !line.startsWith('Done in') &&
+      line.trim() !== '',
+    );
+
+    // console.log('Detected stacks:', stacks);
+    return stacks;
+  } catch (error) {
+    console.error('Error detecting stacks:', error);
+    return [];
+  }
+};
+
+// Function to check if in the code owners file to deploy
+export const checkCodeOwnersProd = (): JobStep[] => [
+  {
+    name: 'Check CODEOWNERS for prod stacks',
+    id: 'check-prod',
+    if: "contains('deploy-${stackName}', 'prod')",
+    env: {
+      GITHUB_TOKEN: '${{ steps.generate-token-prod.outputs.token }}',
+    },
+    run: `
+      CODEOWNERS_FILE=$(cat CODEOWNERS)
+      AUTHOR=$(echo \${{ github.actor }})
+      OWNER=$(echo "$CODEOWNERS_FILE" | grep "prod-only-deployers" | awk '{print $NF}')
+      
+      if [ -z "$OWNER" ]; then
+          echo "No owner found for \${{ github.event.inputs.environment }} environment"
+          exit 1
+      fi
+
+      USER="\${{ github.actor }}"
+          
+      # Check if the owner is a direct user mention
+      if [ "$OWNER" == "@$USER" ]; then
+        echo "User $USER is directly mentioned in CODEOWNERS"
+        exit 0
+      fi
+
+      # Check if the owner is a team
+      if [[ "$OWNER" == @*/* ]]; then
+        # Remove the @ symbol and split org/team
+        TEAM=\${OWNER#@}
+        ORG=\${TEAM%%/*}
+        TEAM=\${TEAM#*/}
+      
+        echo "/orgs/$ORG/teams/$TEAM/memberships/$USER"
+
+        if gh api "/orgs/$ORG/teams/$TEAM/memberships/$USER" --silent; then
+          echo "User $USER is a member of team $OWNER"
+            exit 0
+        fi
+      fi
+          
+      echo "User $USER is not authorized according to CODEOWNERS"
+      exit 1
+    `,
+  },
+];
+
+export const checkCodeOwnersNonProd = (): JobStep[] => [
+  {
+    name: 'Check CODEOWNERS for non-prod stacks',
+    id: 'check-non-prod',
+    if: "!contains('deploy-${stackName}', 'prod')",
+    env: {
+      GITHUB_TOKEN: '${{ steps.generate-token.outputs.token }}',
+    },
+    run: `
+      CODEOWNERS_FILE=$(cat CODEOWNERS)
+      AUTHOR=$(echo \${{ github.actor }})
+      OWNER=$(echo "$CODEOWNERS_FILE" | grep "non-prod-deployers" | awk '{print $NF}')
+      if [ -z "$OWNER" ]; then
+          echo "No owner found for \${{ github.event.inputs.environment }} environment"
+          exit 1
+      fi
+
+      USER="\${{ github.actor }}"
+          
+      # Check if the owner is a direct user mention
+      if [ "$OWNER" == "@$USER" ]; then
+        echo "User $USER is directly mentioned in CODEOWNERS"
+        exit 0
+      fi
+
+      # Check if the owner is a team
+      if [[ "$OWNER" == @*/* ]]; then
+        # Remove the @ symbol and split org/team
+        TEAM=\${OWNER#@}
+        ORG=\${TEAM%%/*}
+        TEAM=\${TEAM#*/}
+           
+        echo "/orgs/$ORG/teams/$TEAM/memberships/$USER"
+        
+        if gh api "/orgs/$ORG/teams/$TEAM/memberships/$USER" --silent; then
+          echo "User $USER is a member of team $OWNER"
+            exit 0
+        fi
+      fi
+          
+      echo "User $USER is not authorized according to CODEOWNERS"
+      exit 1
+    `,
+  },
+];
+
+// Function to run `cdk diff` for a given stack and capture its output
+export const runCdkDiffForStack = (stack: string): JobStep[] => [
+  {
+    name: `Run CDK Diff for ${stack}`,
+    id: `cdk_diff_${stack}`,
+    run: `yarn cdk diff ${stack} > ${stack}.output.txt`,
+  },
+  {
+    name: `Read CDK Diff Output for ${stack}`,
+    id: `read_diff_${stack}`,
+    run: `cat ${stack}.output.txt`,
+  },
+];
+
+// Function to post the output of `cdk diff` as a comment using GitHub Script
+export const postCdkDiffCommentForStack = (stack: string): JobStep[] => [
+  {
+    name: `Post CDK Diff Comment for ${stack}`,
+    uses: `actions/github-script@${githubActionsScriptVersion}`,
+    with: {
+      'github-token': '${{ secrets.GITHUB_TOKEN }}',
+      'script': `
+        const fs = require('fs');
+        const diffOutput = fs.readFileSync('${stack}.output.txt', 'utf8');
+        github.rest.issues.createComment({
+          issue_number: context.issue.number,
+          owner: context.repo.owner,
+          repo: context.repo.repo,
+          body: \`#### ${stack} cdk diff \\\`success\\\`
+<details><summary>Show \\\`cdk diff\\\` Output</summary>
+
+\\\`\\\`\\\`
+\${diffOutput}
+\\\`\\\`\\\`
+
+</details>\`
+        });     
+      `,
+    },
+  },
+];
+
+// Function to generate a GitHub token
+export const generateGitHubToken = (): JobStep[] => [
+  {
+    name: 'Generate GitHub Token',
+    id: 'generate-token',
+    uses: `tibdex/github-app-token@${githubActionsTokenVersion}`,
+    with: {
+      app_id: '${{ secrets.PROJEN_APP_ID }}',
+      private_key: '${{ secrets.PROJEN_APP_PRIVATE_KEY }}',
+    },
+  },
+];
+
+// Function to create common job steps
+const createCommonJobSteps = (): JobStep[] => [
+  { uses: `actions/checkout@${githubActionsCheckoutVersion}` },
+  {
+    uses: `actions/setup-node@${githubActionsSetupNodeVersion}`,
+    with: {
+      'node-version': `${nodeVersion}`,
+    },
+  },
+  { run: 'yarn install --frozen-lockfile' },
+];
+
+// Function to create a workflow for a specific stack
+export const createStackWorkflow = (project: AwsCdkTypeScriptApp, stackName: string) => {
+  const diffDeployWorkflow = new GithubWorkflow(project.github!, `diff-deploy-${stackName}`);
+  diffDeployWorkflow.on({
+    push: {
+      branches: ['**'],
+    },
+    workflowDispatch: {
+      inputs: {
+        diffStack: {
+          description: 'Stack to deploy',
+          required: true,
+          type: 'choice',
+          options: `diff-${stackName}`,
+        },
+        deployStack: {
+          description: 'Stack to deploy',
+          required: true,
+          type: 'choice',
+          options: `deploy-${stackName}`,
+        },
+      },
+    },
+    pullRequest: {
+      types: ['opened', 'synchronize', 'reopened', 'labeled'],
+    },
+  });
+
+  diffDeployWorkflow.addJobs({
+    diff: {
+      runsOn: ['ubuntu-latest'],
+      if: `github.event_name == 'workflow_dispatch' || contains(github.event.pull_request.labels.*.name, 'diff-${stackName}') || github.event.label.name == 'diff-${stackName}'`,
+      permissions: {
+        idToken: JobPermission.WRITE,
+        contents: JobPermission.READ,
+        issues: JobPermission.WRITE,
+        pullRequests: JobPermission.WRITE,
+        repositoryProjects: JobPermission.WRITE,
+      },
+      steps: [
+        ...createCommonJobSteps(),
+        {
+          uses: `aws-actions/configure-aws-credentials@${githubActionsAwsCredentialsVersion}`,
+          with: {
+            'role-to-assume': '${{ secrets.AWS_GITHUB_OIDC_ROLE}}',
+            'aws-region': 'us-east-2', // Adjust this if your stack uses a different region
+          },
+        },
+        ...runCdkDiffForStack(stackName),
+        ...postCdkDiffCommentForStack(stackName),
+      ],
+    },
+    deploy: {
+      needs: ['diff'],
+      runsOn: ['ubuntu-latest'],
+      if: `contains(github.event.pull_request.labels.*.name, 'deploy-${stackName}') || github.event.label.name == 'deploy-${stackName}' || (github.event_name == 'workflow_dispatch')`,
+      permissions: {
+        idToken: JobPermission.WRITE,
+        contents: JobPermission.READ,
+      },
+      steps: [
+        ...createCommonJobSteps(),
+        ...generateGitHubToken(),
+        ...checkCodeOwnersProd(),
+        ...checkCodeOwnersNonProd(),
+        {
+          uses: `aws-actions/configure-aws-credentials@${githubActionsAwsCredentialsVersion}`,
+          with: {
+            'role-to-assume': '${{ secrets.AWS_GITHUB_OIDC_ROLE }}',
+            'aws-region': 'us-east-2', // Adjust this if your stack uses a different region
+          },
+        },
+        { run: `yarn cdk deploy ${stackName} --require-approval never` },
+      ],
+    },
+  });
+};
+      ]==])),
     }),
   },
 })
